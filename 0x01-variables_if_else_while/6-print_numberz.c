e7eef6a4a2f781e5cc1d86adc8da068ce193e1db
Aredina Oluwaseun <seumos@gmail.com>
2022-10-16 02:10:24 -0700

    Variables, if, else and while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers of base 10 starting from 0
+ * Followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 88; n < 99; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
